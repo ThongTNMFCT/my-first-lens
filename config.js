e3da0b822dbf2b07248323dfe0062c599cdb48c0
Trần Ngọc Minh Thông <thongtnmfct31178@gmail.com>
2025-12-28 22:00:14 +0700

    123

---
+++ config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+    firebase: {
+        apiKey: "AIzaSyBLj9J6T7Jk_8-caacVJe1PL87FvfX135Q",
+        authDomain: "my-first-lens.firebaseapp.com",
+        projectId: "my-first-lens",
+        storageBucket: "my-first-lens.firebasestorage.app",
+        messagingSenderId: "794075725078",
+        appId: "1:794075725078:web:f9b0125df8186b50626437"
+    },
+    cloudinary: {
+        cloudName: "de1osc5cg", 
+        uploadPreset: "my-first-lens" 
+    }
+};
